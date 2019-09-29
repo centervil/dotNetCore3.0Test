20d2db3d00b5eb8bdd540f2b7264dade6553c325
ak-nkmr <centervil.en@gmail.com>
2019-09-30 01:25:35 +0900

    UIを作成 #1

--- WpfAppNetCore3Test/WpfAppNetCore3/MainWindow.xaml.cs
+++ WpfAppNetCore3Test/WpfAppNetCore3/MainWindow.xaml.cs
@@ -24,5 +24,20 @@ namespace WpfAppNetCore3
         {
             InitializeComponent();
         }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
