e48c18da67aa60ebf0ed2c132a8a62c2c41175e4
ak-nkmr <centervil.en@gmail.com>
2019-10-07 01:25:20 +0900

    NetStandardライブラリの実装 #1

---
+++ WpfAppNetCore3Test/NetStandardLib/CNetStandard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetStandardLib
+{
+    public class CNetStandard
+    {
+        public static string GetNowTime()
+        {
+            return DateTime.Now.ToString();
+        }
+    }
+}
